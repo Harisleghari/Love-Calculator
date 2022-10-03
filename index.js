c16bbce4410f3c46dc41c5b6f60bbcdd8267c54d
Haris <harisleghari61@gmail.com>
2022-10-03 08:22:21 +0500

    Love Calculator

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//function of RandomNumber
+
 function randomNumber() {
     let num = Math.random() * 100;
     num = Math.floor(num) + 1;
     document.getElementById("randomPercentage").innerHTML = num  + "%";
 }
 
+//Function of reset button
 function resetButton() {
     document.getElementById("randomPercentage").innerHTML = 0 + "%";
 }
